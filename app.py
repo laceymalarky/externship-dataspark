3643ea949fc26c1f1ced0db673a05f1f8b26cfd9
laceymalarky <97048468+laceymalarky@users.noreply.github.com>
2023-10-23 10:32:36 -0500

    Update app

--- app.py
+++ app.py
@@ -10,21 +10,21 @@ import en_core_web_sm
 
 # Streamlit app titles
 st.title(':orange[DataSpeak] Q&A Chatbot💬')
-st.caption(
-    'The bot will return the top 5 possible answers to your question.')
+# st.caption('The bot will return the top 5 possible answers to your question.')
+st.divider()
 
 # Load Data
 df = pd.read_csv(
-    'python_q_a_clean_score3_AandQwc50.csv')
+    'python_q_a_clean_score3_AandQwc150_training.csv')
 df['title_question'] = df.title + '. ' + df.question
 
 # Dowload spacy model
-# spacy.cli.download("en_core_web_sm")
+spacy.cli.download("en_core_web_md")
 
 # Deserialize NLP docs
-nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
+nlp = spacy.load("en_core_web_md", disable=['parser', 'ner'])
 doc_bin = DocBin().from_disk(
-    "python_qa_titlequest.spacy")
+    "python_qa_titlequest_v2.spacy")
 docs = list(doc_bin.get_docs(nlp.vocab))
 
 # User inputs question
@@ -34,11 +34,20 @@ docs = list(doc_bin.get_docs(nlp.vocab))
 # When a user submits a question, generate top 5 answers
 prompt = st.chat_input("Ask a Question")
 if prompt:
-    st.write(f"User has asked the following question: {prompt}")
+    st.markdown(f"**Question**: {prompt}")
 
     input_question = prompt
 
+    # Generate top answer directly from user question
+    generator = pipeline("text2text-generation",
+                         model="lmalarky/flan-t5-base-finetuned-python_qa_v2")
+    answer_t5_main = generator(f"answer the question: {input_question}")
+
+    st.markdown(f"**Answer:** {answer_t5_main[0]['generated_text']}")
+
     # Calculate cosine similarity of input question, return top 5 questions in data
+    st.write(f"Here are 5 additional possible answers:")
+
     similarity = []
     input_text = nlp(input_question)
 
@@ -57,12 +66,12 @@ if prompt:
         output = []
         for context in top5_input:
             generator = pipeline("text2text-generation",
-                                 model="lmalarky/flan-t5-base-finetuned-python_qa")
+                                 model="lmalarky/flan-t5-base-finetuned-python_qa_v2")
             answer_t5 = generator(f"answer the question: {context}")
             output.append(answer_t5[0]['generated_text'])
         return output
 
-    answer_gen_top5 = generate_5_answers(top5['title_question'])
+    answer_gen_top5 = generate_5_answers(top5['question'])
     answer_gen_top5 = pd.DataFrame(answer_gen_top5, columns=['generated_text'])
     top5_gen_answ = answer_gen_top5.join(top5[['similarity']])
 
